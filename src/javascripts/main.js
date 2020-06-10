1d5725794ea43f58783d8f9b0839662d403fdb90
Geno McNew <genemcnew3@gmail.com>
2020-06-10 18:41:56 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,10 +2,12 @@ import 'bootstrap';
 
 import '../styles/styles.scss';
 import multiUsers from './components/multiUsers';
+import navbar from './navbar/navbar';
 
 console.error('HIYA');
 
 const init = () => {
+  navbar.printNav();
   multiUsers.buildMultiUsers();
 };
 
--- src/javascripts/navbar/navbar.js
+++ src/javascripts/navbar/navbar.js
@@ -1,8 +1,14 @@
 import utils from '../helpers/utils';
 import logo from '../assets/onering.png';
+import './navbar.scss';
 
 const printNav = () => {
-  const domString = `<img id="logo" src="${logo} alt="one ring logo"><input type="text" class="text-input" id="messageTextBox"><button class="btn" id="clearButton">Clear Messages</button> `;
+  const domString = `<div class="row d-flex"><a class="navbar-brand offset-2" href="#"><img id="logo" class="center-block" src="${logo}" width="50px" height="50px" alt="one ring logo"></a>
+  <div class="input-group col-6">
+  <input id="messageText" class="form-control m-1" aria-label="With textarea" placeholder="Hit Enter to Send"></input>
+  </div>
+  <button class="m-1 btn btn-light col-1" id="clearButton">Clear Messages</button> 
+  </div>`;
   utils.printToDom('#navbar', domString);
 };
 
